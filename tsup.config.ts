0e9186bf16b9072a0c45052bcf27db7ab4f67cb2
Fábio Luiz <goncalves.flf@gmail.com>
2026-07-31 16:22:40 -0300

    build: config

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['src'],
+    format: ['esm'],
+    outDir: 'build',
+    sourcemap: true,
+    shims: true,
+    target: 'esnext',
+})
